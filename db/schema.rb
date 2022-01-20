daa0d6625a856f2ba80e2723c0ef59dca889dd3b
“TimLit13” <litidm@gmail.com>
2022-01-20 21:36:52 +0300

    Answers body add null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_183231) do
+ActiveRecord::Schema.define(version: 2022_01_20_183405) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body"
+    t.text "body", null: false
     t.integer "question_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
