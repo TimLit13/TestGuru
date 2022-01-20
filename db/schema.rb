bb3559ff0ac6e3a670c1877c91c3350cfc128877
“TimLit13” <litidm@gmail.com>
2022-01-20 21:10:24 +0300

    Add model category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_180757) do
+ActiveRecord::Schema.define(version: 2022_01_20_180950) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2022_01_20_180757) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
