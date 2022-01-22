dcd6f6bcca9bdf8d84acb7c358dbd945f218a7ed
“TimLit13” <litidm@gmail.com>
2022-01-22 13:15:31 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# clear db before seed
+User.delete_all if User.any?
+Answer.delete_all if Answer.any?
+Question.delete_all if Question.any?
+Test.delete_all if Test.any?
+Category.delete_all if Category.any?
+
+# seed categories
+3.times do
+  category_title = Faker::ProgrammingLanguage.name
+  Category.create(title: category_title)
+end
+
+# seed tests
+test_categories = Category.pluck(:id)
+10.times do
+  test_title = Faker::Lorem.sentence(word_count: 4)
+  test_level = rand(1..5)
+  test_category_id = test_categories.sample
+  Test.create(title: test_title, level: test_level, category_id: test_category_id)
+end
+
+# seed questions
+question_tests = Test.pluck(:id)
+30.times do
+  question_body = Faker::Lorem.question
+  question_test_id = question_tests.sample
+  Question.create(body: question_body, test_id: question_test_id)
+end
+
+# seed answers
+answer_questions = Question.pluck(:id)
+30.times do
+  answer_body = Faker::Lorem.paragraph(sentence_count: 2)
+  answer_question_id = answer_questions.sample
+  answer_correct = [true, false].sample
+  Answer.create(body: answer_body, question_id: answer_question_id, correct: answer_correct)
+end
+
+# seed users
+5.times do |i|
+  user_name = Faker::Sports::Football.player
+  user_first_name = user_name.split(' ').first
+  user_last_name = user_name.split(' ').last
+  user_email = Faker::Internet.email
+  user_admin = if i == 1
+                 true
+               else
+                 false
+               end
+  User.create(first_name: user_first_name, last_name: user_last_name, email: user_email, admin: user_admin)
+end
