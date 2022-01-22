325c686aa3e4e251dd0901a41cb45405b9f7a8c3
“TimLit13” <litidm@gmail.com>
2022-01-22 15:18:00 +0300

    Add find tests in category method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  belongs_to :category
+
+  def self.test_titles(category_title)
+    joins(:category).where(categories: {title: category_title}).order(title: :desc).pluck(:title)
+  end
 end
