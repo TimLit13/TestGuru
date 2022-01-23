7e6fe35499ae18ae5201d0bce99a87e862ffbbc4
“TimLit13” <litidm@gmail.com>
2022-01-23 10:04:26 +0300

    Fix seed delete unuseful db queries

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,39 +7,41 @@ Test.delete_all if Test.any?
 Category.delete_all if Category.any?
 
 # seed categories
+categories = []
 3.times do
   category_title = Faker::ProgrammingLanguage.name
-  Category.create(title: category_title)
+  categories.push(Category.create(title: category_title))
 end
 
 # seed tests
-test_categories = Category.pluck(:id)
+tests = []
 10.times do
   test_title = Faker::Lorem.sentence(word_count: 4)
   test_level = rand(1..5)
-  test_category_id = test_categories.sample
-  Test.create(title: test_title, level: test_level, category_id: test_category_id)
+  test_category_id = categories.map(&:id).sample
+  tests.push(Test.create(title: test_title, level: test_level, category_id: test_category_id))
 end
 
 # seed questions
-question_tests = Test.pluck(:id)
+questions = []
 30.times do
   question_body = Faker::Lorem.question
-  question_test_id = question_tests.sample
-  Question.create(body: question_body, test_id: question_test_id)
+  question_test_id = tests.map(&:id).sample
+  questions.push(Question.create(body: question_body, test_id: question_test_id))
 end
 
 # seed answers
-answer_questions = Question.pluck(:id)
+answers = []
 30.times do
   answer_body = Faker::Lorem.paragraph(sentence_count: 2)
-  answer_question_id = answer_questions.sample
+  answer_question_id = questions.map(&:id).sample
   answer_correct = [true, false].sample
   Answer.create(body: answer_body, question_id: answer_question_id, correct: answer_correct)
 end
 
 # seed users
-5.times do |i|
+users = []
+1.upto(5) do |i|
   user_name = Faker::Sports::Football.player
   user_first_name = user_name.split(' ').first
   user_last_name = user_name.split(' ').last
@@ -53,10 +55,8 @@ end
 end
 
 # make references between users and tests
-users = User.pluck(:id)
-tests = Test.pluck(:id)
 Test.count.times do |n| 
-  user_test_user = users.sample
-  user_test_test = tests.sample
+  user_test_user = users.map(&:id).sample
+  user_test_test = tests.map(&:id).sample
   UserTest.create(user_id: user_test_user, test_id: user_test_test)
 end
