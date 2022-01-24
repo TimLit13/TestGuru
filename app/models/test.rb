20288af47a45884a740220bc77fabfc684f5167f
“TimLit13” <litidm@gmail.com>
2022-01-25 00:19:04 +0300

    Add associations between tests and users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,7 @@
 class Test < ApplicationRecord
   has_many :questions
+  has_many :users, through: :user_tests 
+  belongs_to :user, foreign_key: :author_id, class_name: 'User'
   belongs_to :category
 
   def self.test_titles(category_title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-
+  has_many :tests
+  has_many :tests, through: :user_tests
   def tests_by_level(level)
     UserTest.where(user_id: id).joins(:test).where("tests.level = ?", level).pluck(:title)
   end
