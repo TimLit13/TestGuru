c24848300ae1fff26d55a348ac7fe33b047687b7
“TimLit13” <litidm@gmail.com>
2022-01-25 22:56:07 +0300

    Fix stub for Test link to Author

--- db/migrate/20220124195211_add_user_ref_to_tests.rb
+++ db/migrate/20220124195211_add_user_ref_to_tests.rb
@@ -1,6 +1,5 @@
 class AddUserRefToTests < ActiveRecord::Migration[7.0]
   def change
-    add_reference :tests, :user, foreign_key: true
-    rename_column :tests, :user_id, :author_id
+    add_reference :tests, :author, foreign_key: { to_table: :users }
   end
 end
