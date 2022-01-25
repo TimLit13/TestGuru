3a8e4f25f57b94dc8cf550764e9f81c85bd55980
“TimLit13” <litidm@gmail.com>
2022-01-25 22:42:36 +0300

    Add missing association for user_tests

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   has_many :questions
+  has_many :user_tests
   has_many :users, through: :user_tests 
   belongs_to :user, foreign_key: :author_id, class_name: 'User'
   belongs_to :category
