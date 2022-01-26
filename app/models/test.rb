1101447bf1d913ec78567d70ce8c849563f66c83
“TimLit13” <litidm@gmail.com>
2022-01-26 20:06:37 +0300

    Rename association for test author

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :user_tests
   has_many :users, through: :user_tests 
-  belongs_to :user, foreign_key: :author_id, class_name: 'User'
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :category
 
   def self.test_titles(category_title)
