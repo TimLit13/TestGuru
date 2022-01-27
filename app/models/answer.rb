ac365d8a22ea841055ae6a155f622a3a006c9c17
“TimLit13” <litidm@gmail.com>
2022-01-27 23:18:39 +0300

    Add validation for min 1 and max 4 answers for question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,29 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :body, presence: true
+  before_destroy :check_min_answers
 
-   scope :correct_answers, -> { where(correct: true) }
+  validates :body, presence: true
+  validate :validate_max_answers, on: [:create, :save]
+
+  scope :correct_answers, -> { where(correct: true) }
+
+  private
+
+  def validate_max_answers
+    errors.add(
+      :max_answers, 
+      "Only 4 answers for question available"
+    ) if question.answers.count >= 4
+  end
+  
+  def check_min_answers
+    if question.answers.count == 1
+      errors.add(
+      :min_answers,
+      "Should be more than 1 answer"
+      ) 
+      throw(:abort)
+    end
+  end
 end
