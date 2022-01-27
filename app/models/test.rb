1f27410a4cea4dcfee2a1d554f6f036c0d280629
“TimLit13” <litidm@gmail.com>
2022-01-27 20:48:14 +0300

    Add scopes for taking test by level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: 'User'
   belongs_to :category
 
+  scope :easy_tests, -> { where(level: 0..1) }
+  scope :average_tests, -> { where(level: 2..4) }
+  scope :difficult_tests, -> { where(level: 5..Float::INFINITY) }
+
   def self.test_titles(category_title)
     joins(:category).where(categories: {title: category_title}).order(title: :desc).pluck(:title)
   end
