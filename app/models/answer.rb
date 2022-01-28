73a553cef38d4b204ef9958c54aee5d62a3ce57f
“TimLit13” <litidm@gmail.com>
2022-01-28 19:17:23 +0300

    Fix Answer validations delete unuseful validation method

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,8 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  before_destroy :check_min_answers
-
   validates :body, presence: true
-  validate :validate_max_answers, on: [:create, :save]
+  validate :validate_max_answers, on: :create
 
   scope :correct_answers, -> { where(correct: true) }
 
@@ -13,17 +11,8 @@ class Answer < ApplicationRecord
   def validate_max_answers
     errors.add(
       :max_answers, 
-      "Only 4 answers for question available"
+      'Only 4 answers for question available'
     ) if question.answers.count >= 4
   end
   
-  def check_min_answers
-    if question.answers.count == 1
-      errors.add(
-      :min_answers,
-      "Should be more than 1 answer"
-      ) 
-      throw(:abort)
-    end
-  end
 end
