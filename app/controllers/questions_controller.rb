fc40da7c8d813a4a583ee1a89291d634c8313d5c
“TimLit13” <litidm@gmail.com>
2022-01-30 16:58:26 +0300

    Add rendering question action show

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,13 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   def index
     @questions = @test.questions
   end
 
   def show
+    render plain: @question.body
   end
 
   def create
@@ -16,11 +18,15 @@ class QuestionsController < ApplicationController
 
   private
 
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
   def find_test
     @test = Test.find(params[:test_id])
   end
 
-  def question_params
-    params.require(:question).permit(:body)
-  end
+  # def question_params
+  #   params.require(:question).permit(:body)
+  # end
 end
