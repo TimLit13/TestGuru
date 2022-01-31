f4163c376eca13bbd3284085c2ae129f9b36b4c2
“TimLit13” <litidm@gmail.com>
2022-02-01 00:15:05 +0300

    Fix views and add links

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -37,7 +37,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     if @question.destroy
-      redirect_to test_questions_path(@question.test)
+      redirect_to @question.test
     else
       render :edit
     end
