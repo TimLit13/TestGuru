a4a1019943396496d185758130aabbf6f54c6da0
“TimLit13” <litidm@gmail.com>
2022-02-01 19:45:46 +0300

    Refactor delete unuseful callbacks

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,24 +1,16 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[show]
-  before_action :find_all_tests, only: %i[index]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
   def index
+    @tests = Test.all
   end
 
   def show
-  end
-
-  private
-
-  def find_test
     @test = Test.find(params[:id])
   end
 
-  def find_all_tests
-    @tests = Test.all
-  end
+  private
 
   def rescue_with_record_not_found
     render inline: "Can't find test with id: #{params[:id]} [status: 404]"
