8a05dbf3b4193b7359758c30124f2e09181be34a
“TimLit13” <litidm@gmail.com>
2022-02-05 14:35:01 +0300

    Add methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_year
+    Time.now.year
+  end
+
+  def link_to_github(author, repo)
+    link_to 'Github', "https://github.com/#{author}/#{repo}", target: :_blank
+  end
 end
