154344ef9b93ccbfd47d7cafee5e38add78712fc
“TimLit13” <litidm@gmail.com>
2022-02-05 16:12:31 +0300

    Add seeds and fix category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :tests
 
-  validates :body, presence: true
+  validates :title, presence: true
 
   default_scope { order(title: :asc) } 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-CATEGORIES_SEED = 3
-TESTS_SEED = 10
-QUESTIONS_SEED = 30
-ANSWERS_SEED = 30
-USERS_SEED = 5
+CATEGORIES_SEED = 2
+TESTS_SEED = 5
+QUESTIONS_SEED = 10
+ANSWERS_SEED = 10
+USERS_SEED = 3
 
 # clear db before seed
 UserTest.delete_all if UserTest.any?
@@ -19,13 +19,29 @@ CATEGORIES_SEED.times do
   categories.push(Category.create(title: category_title))
 end
 
+# seed users
+users = []
+1.upto(USERS_SEED) do |i|
+  user_name = Faker::Sports::Football.player
+  user_first_name = user_name.split(' ').first
+  user_last_name = user_name.split(' ').last
+  user_email = Faker::Internet.email
+  user_admin = if i == 1
+                 true
+               else
+                 false
+               end
+  users.push User.create(first_name: user_first_name, last_name: user_last_name, email: user_email, admin: user_admin)
+end
+
 # seed tests
 tests = []
 TESTS_SEED.times do
   test_title = Faker::Lorem.sentence(word_count: 4)
   test_level = rand(1..5)
   test_category_id = categories.map(&:id).sample
-  tests.push(Test.create(title: test_title, level: test_level, category_id: test_category_id))
+  test_author = User.first
+  tests.push(Test.create!(title: test_title, level: test_level, category_id: test_category_id, author: test_author))
 end
 
 # seed questions
@@ -43,21 +59,6 @@ ANSWERS_SEED.times do
   answer_question_id = questions.map(&:id).sample
   answer_correct = [true, false].sample
   answers.push Answer.create(body: answer_body, question_id: answer_question_id, correct: answer_correct)
-end
-
-# seed users
-users = []
-1.upto(USERS_SEED) do |i|
-  user_name = Faker::Sports::Football.player
-  user_first_name = user_name.split(' ').first
-  user_last_name = user_name.split(' ').last
-  user_email = Faker::Internet.email
-  user_admin = if i == 1
-                 true
-               else
-                 false
-               end
-  users.push User.create(first_name: user_first_name, last_name: user_last_name, email: user_email, admin: user_admin)
 end
 
 # make references between users and tests
