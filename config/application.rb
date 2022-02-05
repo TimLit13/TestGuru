a98fb359a539da426a50d07b3fee5330e841f684
“TimLit13” <litidm@gmail.com>
2022-02-05 12:57:40 +0300

    Fix config

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,11 @@ module TestGuru
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    
+    config.time_zone = "St. Petersburg"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.encoding = "utf-8"
+    config.i18n.default_locale = :ru
   end
 end
