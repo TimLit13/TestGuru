c83253413d0aebfda41b14711999ffa761e05658
“TimLit13” <litidm@gmail.com>
2022-02-09 21:04:12 +0300

    Make next question method as callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,11 +4,10 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_set_next_question
 
   def accept!(answer_ids)
     self.correct_question += 1 if correct_answer?(answer_ids)
-
-    self.current_question = next_question
     save!
   end
 
@@ -23,9 +22,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    correct_answers_count == correct_answers.where(id: answer_ids).count && correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
@@ -34,6 +31,10 @@ class TestPassage < ApplicationRecord
 
   def next_question
     test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def before_update_set_next_question
+    self.current_question = next_question
   end
 
 end
