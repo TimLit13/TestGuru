c15536ebf0d1c4504a259dbe212db4ff5b598c0d
“TimLit13” <litidm@gmail.com>
2022-02-09 21:30:38 +0300

    Add style for complete test and link to all tests

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,15 @@
 module TestPassagesHelper
+  TEST_PASSAGE_SUCCESS = 0.85
+
+  def test_passage_success?(test_passage)
+    if test_passage_score(test_passage) >= TEST_PASSAGE_SUCCESS
+      true
+    else
+      false
+    end
+  end
+
+  def test_passage_score(test_passage)
+    test_passage.correct_question/test_passage.test.questions.count.to_f.round(2)
+  end
 end
