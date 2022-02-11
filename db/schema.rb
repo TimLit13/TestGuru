0803ca3992989a749c04b881803e7ff4defd0150
“TimLit13” <litidm@gmail.com>
2022-02-11 20:05:13 +0300

    Add password to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_201302) do
+ActiveRecord::Schema.define(version: 2022_02_11_170306) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_201302) do
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
