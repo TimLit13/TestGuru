54d83e2a6d6b22c7d0a7a24d056021dde83d0079
“TimLit13” <litidm@gmail.com>
2022-02-12 13:20:17 +0300

    Add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
   
+  EMAIL_FORMAT = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, foreign_key: :author_id, class_name: 'Test'
   
   has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: EMAIL_FORMAT, message: "не является email"}
+  validates :email, uniqueness: true
 
   def tests_by_level(level)
     UserTest.where(user_id: id).joins(:test).where("tests.level = ?", level).pluck(:title)
@@ -15,5 +21,4 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(created_at: :desc).find_by(test_id: test.id)
   end
-
 end
