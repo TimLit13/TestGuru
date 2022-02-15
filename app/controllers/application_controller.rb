c8f4d0f6421e8336b2e7433713f09619bf5e5287
“TimLit13” <litidm@gmail.com>
2022-02-16 00:13:41 +0300

    Add flash message for registered admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(current_user)
+    flash[:notice] = "Welcome, #{current_user.first_name}" if current_user.sign_in_count == 1
     current_user.is_a?(Admin) ? admin_tests_path : root_path
   end
 
