111c3c5deae83b5dbeca2935169742e838863a8b
“TimLit13” <litidm@gmail.com>
2022-02-16 21:55:51 +0300

    Fix greeting for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(current_user)
-    flash[:notice] = "Welcome, #{current_user.first_name}" if current_user.sign_in_count == 1
-    current_user.is_a?(Admin) ? admin_tests_path : root_path
+    flash[:notice] = "Welcome, #{current_user.first_name}"
+    current_user.admin? ? admin_tests_path : root_path
   end
 
   protected
