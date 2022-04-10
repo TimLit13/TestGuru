e2ec065d44c261126f1b2d6395dafa1039f8b0eb
“TimLit13” <litidm@gmail.com>
2022-04-10 12:33:23 +0300

    Add timer for test and fix views for admin

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -72,6 +72,6 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def tests_params
-    params.require(:test).permit(:title, :level, :category_id, :ready)
+    params.require(:test).permit(:title, :level, :category_id, :ready, :timer)
   end
 end
